2addfb9fda7bc6e80f1021ae607092bfb11d58f4
Sreenath k k <sreenathkk00@gmail.com>
2024-02-08 23:38:00 +0530

    singup  page created

--- user_app/urls.py
+++ user_app/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from user_app import views
 
+from django.urls import path
+from user_app import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('user_app.urls')),
+    path('',views.signup,name='signup'),
 ]
