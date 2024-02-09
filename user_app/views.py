ead6e55d8748ee323d05394d0406f4b9c6e553e9
Sreenath k k <sreenathkk00@gmail.com>
2024-02-10 00:11:31 +0530

    try and except for signup

--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
-    return render(request,'signup.html')
+    user=None
+    error_massage =None
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username=username,password=password)
+        except :
+            error_massage= 'username alredy existing'
+    return render(request,'signup.html',{'user':user,'error_massage':error_massage})
