64f34ef2c86e31dfe64975e991d01a69e09a4533
Sreenath k k <sreenathkk00@gmail.com>
2024-12-23 13:38:29 +0530

    Refactor URL configuration for enhanced authentication and password management
    - Replaced old URL paths for `signin` and `signup` with the newly created `CustomLoginView` and `signup_view` for better form handling and custom behavior.
    - Updated home and dashboard routes to match the new app structure, providing better organization and user navigation.
    - Introduced paths for password reset functionality, including `password_reset_view`, `password_reset_done`, and `password_reset_confirm` with custom templates for enhanced user experience.
    - Used Django’s built-in `LogoutView` and `PasswordResetDoneView` for standard logout and password reset completion flows.
    - Organized routes for social authentication with `social_django` and added password reset completion and confirmation paths.

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,27 +1,17 @@
-"""
-URL configuration for user_management project.
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path
-from user_app import views
-from django.conf.urls import include
 urlpatterns = [
-    path('home/',views.home,name='home'),
-    path('signup/',views.signup,name='signup'),
-    path('',views.signin,name='signin'),
-    path('signout/',views.signout,name='signout'),
+    path('',views.home,name='home'),
+    path('dashboard/',views.dashboard ,name='dashboard'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset_view/',views.password_reset_view,name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('resert/<uidb64>/<token>/',views.password_reset_confirm,name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
+    # google auth
     path('social-auth/', include('social_django.urls', namespace='social')),
 ]
